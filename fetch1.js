9df5e19301a41bffcc66dd0dd24fc398a75ff460
nahyun park <pnh13584@gmail.com>
2025-01-14 15:05:27 +0900

    fetch 오류 해결

--- fetch1.js
+++ fetch1.js
@@ -1,5 +1,4 @@
-const resultUl=document.querySelector("#result");
-const cards=document.querySelector(".makecards")
+
 
 const options = {
   method: 'GET',
@@ -9,25 +8,76 @@ const options = {
   }
 };
 
-let url="https://api.themoviedb.org/3/movie/popular?api_key=3ee09de682c96ebde809ab5428f309c7&language=ko-KR&page=1&region=KR"
+// fetch url 
+let url="https://api.themoviedb.org/3/movie/popular?api_key=${}&language=ko-KR&page=1&region=KR"
 
-let temp_html ;
+let imgURl="https://image.tmdb.org/t/p/w500/";
 
+// fetch 데이터 받아서 정리 
 fetch(url, options)
-  .then(res => res.json())
-  // await async로 고치기 
-  // map이나 foreach 등으로 카드 함수를 부르기
-  .then((res1) => {
-    res1.forEach((data) => {
+  .then((response) => response.json())
+  .then((data) => {
+    data.results.forEach((moviedata) => {
+      console.log(`moviedata: ${JSON.stringify(moviedata,null,4)}`)
+      document.querySelector(".container").innerHTML += `
+       <div class ="item">
+            <img src="${imgURl}${moviedata.poster_path}" alt="..." id="card_image">
+            <h3 class="card_title">${moviedata.title}</h3>
+            <p class= "card_rank">${moviedata.vote_average}</p>
+        </div>`;
+            }).catch((error) =>
+            document.querySelector(".container").innerHTML += `
+            <div>
+                <div>Error occurred.</div>
+            </div>
+          `)
+  })
+  
 
-      // 카드 함수 따로 만들기
-     
-    });    
-  });  
+// const getData = async () => {
+//     const response = await fetch(url, options);
+//     const moviedata = await response.json();
+//     const cardList = document.createDocumentFragment();
+//     Array.from(moviedata).forEach((item) => {
+//       const stagedItem = document.createElement("li");
+//       stagedItem.dataset.img = item.poster_path;
+//       stagedItem.dataset.title = item.title;
+//       stagedItem.dataset.average = vote_average;
+//       stagedItem.classList ="list";
+//       stagedItem.innerHTML = `<div class="item">
+//                     <img src= "${item.img}" alt="...">
+//                     <h3>${item.title}</h3>
+//                     <p>${item.average}</p>
+//                 </div>`;
+//       cardList.appendChild(stagedItem);
+//     });    
+//     makeCards.appendChild(cardList);
+//     return moviedata;
+// };
 
-    resultUl.innerHTML = temp_html;
+// getData();
 
 
+
+
+// newDiv.innerHtml =`<img > ....`
+//   return newDiv;
+
+// //  fetch 불러오기 
+// const options = {
+//   method: 'GET',
+//   headers: {
+//     accept: 'application/json',
+//     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzZWUwOWRlNjgyYzk2ZWJkZTgwOWFiNTQyOGYzMDljNyIsIm5iZiI6MTczNjI5ODk1OC4zNzksInN1YiI6IjY3N2RkMWNlMjAzN2ZmMDRjOTRlNTM3OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.kIyJ1LiE84yox34iMb0uE403wJ4zbd5aKerZ30GG7P8'
+//   }
+// };
+
+// fetch(url, options)
+//   .then(res => res.json())
+//   .then((data) => {
+//     const movie = data['results'];
+//     const Movielist = document.querySelector("");
+//   })
 
 //  await /async 로 다고치고 
 // 카드를 만드는 함수를 따로 만든다
